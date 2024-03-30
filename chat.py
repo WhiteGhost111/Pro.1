34d4fd7a1105955752702c6d2ac411687329823b
nikt <142347330+WhiteGhost111@users.noreply.github.com>
2024-03-30 09:39:57 +0100

    Gra-chat.py
    Dodanie gry w zgadywanie z dwoma błędam + błąd import

--- chat.py
+++ chat.py
@@ -1,6 +1,3 @@
-import discord
-from settings import setting
-from bot_logic import *
 # Zmienna intencje przechowuje uprawnienia bota
 intents = discord.Intents.default()
 # Włączanie uprawnienia do czytania wiadomości
@@ -22,7 +19,7 @@ async def on_message(message):
         await message.channel.send("Cześć!")
     elif message.content.startswith('$bye'):
         await message.channel.send("\\U0001f642")
-    #elif message.content.startswith('$password'):    - import nie działa
+    #elif message.content.startswith('$password'):
         #await message.channel.send(haslo(10))
     elif message.content.startswith('co tam'):
         await message.channel.send("nie narzekam")
@@ -32,6 +29,23 @@ async def on_message(message):
         else:
             count_heh = 5
         await message.channel.send("he" * count_heh)
+#    elif message.content.startswith('$guess'):    - Dodanie gry 
+#          await message.channel.send('Guess a number between 1 and 10.')
+#    
+#           def is_correct(m):
+#               return m.author == message.author and m.content.isdigit()
+#
+#           answer = random.randint(1, 10)
+#
+#           try:
+#               guess = await self.wait_for('message', check=is_correct, timeout=5.0)    - błąd "self"
+#           except asyncio.TimeoutError:                                                 - Błąd "asyncio"
+#               return await message.channel.send(f'Sorry, you took too long it was {answer}.')
+#
+#           if int(guess.content) == answer:
+#               await message.channel.send('You are right!')
+#            else:
+#                await message.channel.send(f'Oops. It is actually {answer}.')
     else:
         await message.channel.send(message.content)
 
