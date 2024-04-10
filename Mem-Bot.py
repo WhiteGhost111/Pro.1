720f3bf42e7cc095b9c23e56c94b946d6aefa0fb
nikt <142347330+WhiteGhost111@users.noreply.github.com>
2024-04-10 17:09:24 +0200

    Update Mem-Bot.py
    cały kod

--- Mem-Bot.py
+++ Mem-Bot.py
@@ -1,3 +1,35 @@
+import discord
+from discord.ext import commands
+import random
+from settings import setting
+import os
+import asyncio
+import requests
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+#@bot.command()
+#async def mem(ctx):
+ ##   with open('images/mem1.jpg', 'rb') as f:
+ #       # Zapiszmy przekonwertowany plik biblioteki Discord w tej zmiennej!
+#        picture = discord.File(f)
+# Możemy następnie wysłać ten plik jako parametr!
+ #   await ctx.send(file=picture)
+
 @bot.command()
 async def mem(ctx):
     images_folder = 'images'
@@ -23,3 +55,5 @@ async def duck(ctx):
     '''Po wywołaniu polecenia duck program wywołuje funkcję get_duck_image_url'''
     image_url = get_duck_image_url()
     await ctx.send(image_url)
+
+bot.run(setting['TOKEN'])
